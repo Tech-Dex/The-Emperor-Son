04b9a928ec9fb972679178ff7b2fc3026445e405
Dexter <rominu.alexandru@yahoo.com>
2019-06-07 17:46:13 +0300

    Retard Mode

---
+++ The Emperor's Son/Assets/Scripts/ScreenUpSideDown.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class ScreenUpSideDown : MonoBehaviour
+{
+
+    public Material effectMaterial;
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, effectMaterial);
+    }
+}
+
